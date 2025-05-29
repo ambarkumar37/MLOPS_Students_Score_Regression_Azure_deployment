4b88cf445021598a56663bbf310478f5a27af119
ambarkumar37 <ambarkumar37@gmail.com>
2025-05-29 16:59:09 +0530

    setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="mlproject",
     version="0.0.1",
-    author="Krish",
-    author_email="krishnaik06@gmail.com",
+    author="ambar",
+    author_email="ambarkumar37@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
